001cf8cfc72e743f4e9c7fa41add2805378e665d
crys00n <crys00n9712@gmail.com>
2024-09-27 20:04:16 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,14 @@
-def text():
+"""Module providing a function printing 'hello world!'."""
+
+
+def text(m):
+    """Returns sum of three strings (takes first of them).
+
+    Returns:
+        message (str): string of the sum of strings m and two given: " " and "world!"
+    """
+    message = m + " " + "world!"
     return message
+
+
+print(text("hello"))
