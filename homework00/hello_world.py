2789b5ad4343ca7eeff668bd72d74b2a5fd8a04f
crys00n <crys00n9712@gmail.com>
2024-09-29 18:48:27 +0300

    упростил функцию

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,14 +1,13 @@
 """Module providing a function printing 'hello world!'."""
 
 
-def text(m):
+def text(message="hello world!"):
     """Returns sum of three strings (takes first of them).
 
     Returns:
         message (str): string of the sum of strings m and two given: " " and "world!"
     """
-    message = m + " " + "world!"
     return message
 
 
-print(text("hello"))
+print(text())
